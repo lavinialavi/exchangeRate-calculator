f0a1ee7838892cd588f4ba594d5bd5663427ed23
lavinia <laviniutza_90@yahoo.com>
2020-02-06 17:37:26 +0200

    add one version of project

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function calculateExchangeRate() {
     console.log(input3);
     var input2 = document.getElementById("input2").value;
     console.log(input2);
-    var result = document.getElementById("result").value =   calculate(input1, input2, input3); 
-   
+    document.getElementById("result").value = calculate(input1, input2, input3);
+
 }
 
 document.getElementById("input2").addEventListener("change", calculateExchangeRate);
@@ -25,15 +25,55 @@ function calculate(from, to, amount) {
     const EUR = {
         RON: 4.79,
         USD: 1.2,
-        EURO: 1
+        EUR: 1
 
     }
-    if (from == "EUR") {
-        result = EUR[to] * amount;
+
+    const RON = {
+        RON: 1,
+        USD: 0.23,
+        EUR: 0.21
     }
 
+    const USD = {
+        USD: 1,
+        EUR: 0.91,
+        RON: 4.33
+    }
+    const currencys = {
+
+        EUR: {
+            RON: 4.79,
+            USD: 1.2,
+            EUR: 1
+        },
+
+        RON: {
+            RON: 1,
+            USD: 0.23,
+            EUR: 0.21
+        },
+
+        USD: {
+            USD: 1,
+            EUR: 0.91,
+            RON: 4.33
+        }
+
+    }
+ result= currencys[from][to]* amount;
+
+    // if (from == "EUR") {
+    //     result = EUR[to] * amount;
+    // }
+
+    // if (from == "RON") {
+    //     result = RON[to] * amount;
+    // }
+    // if (from == "USD") {
+    //     result = USD[to] * amount;
+    // }
     return result;
-
 
 
 }
